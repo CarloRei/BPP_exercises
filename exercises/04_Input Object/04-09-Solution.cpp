e19de40fbb8a6f23e960625b7a074d169f39abdb
CarloRei <77672785+CarloRei@users.noreply.github.com>
2023-01-17 16:06:54 +0100

    aufgaben 04

---
+++ exercises/04_Input Object/04-09-Solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+double number, length, width, height;
+
+
+cin>> number;
+cin>> length >> width >> height;
+
+cout<<number<<endl;
+cout<<length<<endl<<width<<endl<<height<<endl;
+
+
+
+
+
+  return 0;
+}
